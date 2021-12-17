a4ee5dbbde553f94dd67582a2d6e82d713e5271a
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2021-12-17 15:32:16 +0530

    uploaded even number checker..

---
+++ c#(c sharp)/even number checker/even_num.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace even_num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // working...success..
+            int user;
+            String tmp;
+            Console.WriteLine("today we will check if a number is even or not..\n enter a number");
+            tmp=Console.ReadLine();
+            user=Convert.ToInt32(tmp);
+            String ans=(user%2==0)?"even number":"not even number";
+            Console.WriteLine(ans);
+        }
+    }
+}
