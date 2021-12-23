3d00f0ac01e427d72ba9e0cba02025eafd8f4478
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2021-12-23 13:16:09 +0530

    uploaded src folder
    I am only uploading the src folders...i may upload the other folder later

---
+++ react projects/app1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>Hello there</p>
+       </header>
+    </div>
+  );
+}
+
+export default App;
