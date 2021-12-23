02b3edf1bf0f8ed5b9bc4920ed39a1a3a5936c38
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2021-12-23 15:05:11 +0530

    Create program_two.cs

---
+++ c#(c sharp)/multidimensional/program_two.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace multi_dimensional2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//working...success....
+             Console.WriteLine("enter 2 values");
+            int row=Convert.ToInt32(Console.ReadLine()),column=Convert.ToInt32(Console.ReadLine());
+            int[ , ] x=new int[row,column];
+            for(int i=0;i<row;i++){
+                for(int j=0;j<column;j++){
+                    Console.WriteLine("enter element at Row {0} Column {1}",i,j);
+                    x[i,j]=Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("\n \n");
+            for(int i=0;i<row;i++){
+                for(int j=0;j<column;j++){
+                  Console.Write("{0}  ",x[i,j]); 
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
