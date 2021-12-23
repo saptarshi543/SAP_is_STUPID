18505bb87dd2915933767bd46035d8e8b8f54a59
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2021-12-23 14:48:21 +0530

    Create Program.cs

---
+++ c#(c sharp)/alias/Program.cs
@@ -0,0 +1,14 @@
+using m=System.Math;
+using m2=System.Console;
+
+namespace Alias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i=m.Pow(12,4);
+            m2.WriteLine("hi there!!\ni={0}",i);
+        }
+    }
+}
