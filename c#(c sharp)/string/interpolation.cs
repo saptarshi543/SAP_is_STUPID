f57c55de0e5dac8596486c9b6bbd2eb4695bae43
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2021-12-23 15:12:24 +0530

    Create interpolation.cs

---
+++ c#(c sharp)/string/interpolation.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace interpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name="hello there!";
+            string message=$"{name} \n General Kenobi";
+            Console.WriteLine(message);
+        }
+    }
+}
