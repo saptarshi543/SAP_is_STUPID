d5f8fe72eb2e1856469c74d5d61ee81467d6cf3b
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2021-12-23 13:31:40 +0530

    created a new file and imported it in app

---
+++ react projects/app1/src/function1.js
@@ -0,0 +1,11 @@
+import React from "react";
+import reactDom from "react-dom";
+
+function display(props){
+    return (
+        <>
+        <h1>hi there</h1>
+        </>
+    );
+}
+export default display;
