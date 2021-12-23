54d8019f79a858334cc9a9bd7bb6a3981cdeecd6
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2021-12-23 13:32:49 +0530

    added display tag
    imported a file from outside the file

--- react projects/app1/src/App.js
+++ react projects/app1/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import display from './function1';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <p>Hello there</p>
+    <display/>
        </header>
     </div>
   );
