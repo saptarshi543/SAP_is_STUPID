85087d85c385cf299c6995a2320b4e8e2bec4f6f
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-01-06 13:25:55 +0530

    deleted because i created a new repo about it

--- GO/array1.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var arr1 = new([5]int)
-	fmt.Println(arr1[2])
-	fmt.Println(arr1[0])
-}
--- GO/float.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	price := 5.00000
-	vat := 0.7503224000000
-	fmt.Printf("Price: %.2f\nVAT: %.5f", price, vat)
-}
