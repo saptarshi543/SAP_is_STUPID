fa499360a1e04a014d03a2154234ad1b593ee546
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-01-27 13:16:04 +0530

    Update binarySearch.java

--- java/binarySearch.java
+++ java/binarySearch.java
@@ -1,37 +1,34 @@
 //binary search
 import java.util.*;
-public class binarySearch{//not working....
-public static int binary(int arr[], int first, int last, int key){  
-        if (last>=first){  
-            int mid = first + (last - first)/2;  
-            if (arr[mid] == key){  
-            return mid;  
-            }  
-            if (arr[mid] > key){  
-            return binary(arr, first, mid-1, key);//search in left subarray  
-            }else{  
-            return binary(arr, mid+1, last, key);//search in right subarray  
-            }  
-        }  
-        return -1;  
-    } 
-	public static void main(String h[]){
-		System.out.println("we will binary sort an aray..\nenter no. of entries");
-	Scanner nrt=new Scanner(System.in);
-	int n=nrt.nextInt();
-	System.out.println("what do you want to search?");
-	int se=nrt.nextInt();
-	int g[]=new int [n];
-	for (int f=0;f<n ;f++ ) {
-		System.out.print("enter element");
-		g[f]=(nrt.nextInt());
-	}
-	int last=g.length-1;
-    int result=binary(g,0,last,se);
-    if (result<=-1) {
-    	System.out.println("element not found..");
+public class binarySearch{
+    // working....
+    public static int  binary_search(int target, int[] database){
+        int start=0,end= database.length -1;
+        while(start<=end){
+            int mid= (start + end)/2;
+            if(target== database[mid]){
+                return mid;
+            }else if(target < database[mid]){
+                end= mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return -1;
     }
-    else
-    	System.out.println("element found at : "+result);
-	}
+
+    public static void main(String d[]){
+        Scanner nrt=new Scanner(System.in);
+        System.out.println("enter no. of inputs: ");
+        int no_of_inputs=nrt.nextInt();
+        int user[]=new int[no_of_inputs];
+        System.out.println("enter the elements\nplease enter in an increasing or decreasing order");
+        for(int itr=0;itr<no_of_inputs;itr++){
+            user[itr]=nrt.nextInt();
+        }
+        System.out.println("enter your element to search");
+        int target=nrt.nextInt();
+        int result_index=binary_search(target,user);
+        System.out.println("index: "+(result_index-1)+"\n !! if result is negative that implies element is not found in the array !!");
+    }
 }
