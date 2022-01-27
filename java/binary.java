7b73e997b7474f9fd0d32f11e67a1ae097b594e9
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-01-27 13:15:21 +0530

    Update binary.java

--- java/binary.java
+++ java/binary.java
@@ -1,34 +1,21 @@
 import java.util.*;
-import java.lang.Math;
 public class binary{
-//its working....but i will analyse it later for efficiency
-    public static int binaryS(int a[],int key){
-        int low = 0,high=a.length-1;
-        int index=0;
-        while(low<=high){
-            int mid=low+ (high-low)/2;
-            if(a[mid]==key)
+    // working....
+    public static int BinarySearch(int arr[], int first, int last, int element){
+        int mid=(first+last)/2;
+        while(first<=last){
+            if(arr[mid]<element){
+                first=mid+1;
+            }else if(arr[mid]==element){
                 return mid;
-            if(a[mid]<key)
-                low=mid+1;
-            else
-                high=mid-1;
+            }else{last=mid-1;}
+            mid=(first+last)/2;
         }
         return -1;
     }
-    public static void main(String s[]){
-    //let's do binary search...
-        Scanner nrt=new Scanner(System.in);
-        System.out.println("enter no.of terms");
-        int n=nrt.nextInt();
-        int user[]=new int[n];
-        for(int r=0;r<n;r++){user[r]=nrt.nextInt();}
-        System.out.println("what do you want to search...");
-        int t=nrt.nextInt();
-        nrt.close();
-        Arrays.sort(user);
-        int result=binaryS(user,t);
-        if(result==-1){System.out.println("not found");}
-        else
-            System.out.println("element found at index "+result);
-}}
+    public static void main(String a[]){
+        int A[]={12,14,34,56,57,67,74,83,89,90,91};
+        int index=BinarySearch(A,0,(A.length-1),89);
+        System.out.println("index: "+index);
+    }
+}
