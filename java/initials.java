e4c11b12ab7f3d67eebcbe3e376697a3dc63bd6e
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-02-22 11:56:02 +0530

    removed unnecessary print statement

--- java/initials.java
+++ java/initials.java
@@ -13,15 +13,13 @@ import java.util.*;
 public class initials{
 	public static void main(String l[]){
 		Scanner nrt=new Scanner(System.in);
-		System.out.println("enter your stupid name>>");
+		System.out.println("enter your name");
 		String name=((nrt.nextLine()).toUpperCase()).trim(),ans="";
 		// assuming input: "Sap Is Stupid"
 		int last_whitespace=name.lastIndexOf(" ");
 		ans+=(name.substring(last_whitespace,name.length())+" "+((name.charAt(0)))+". ").trim();// returns "Stupid S."
-
-		System.out.println(ans);
 		
-		for(int i=0;i<last_whitespace;i++){
+		for(int i=0;i<last_whitespace;i++){//this loop takes care of rest of the words in the full name if any
 			if(name.charAt(i)==' '){
 				ans+= name.charAt(i+1)+".";
 			}
