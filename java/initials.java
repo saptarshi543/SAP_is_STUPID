365e2169e51138214d595587d26cfe058eb05f7d
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-02-22 11:54:09 +0530

    Create initials.java

---
+++ java/initials.java
@@ -0,0 +1,31 @@
+/*
+input: Abcd Efr Xy
+output: Xy A.E.
+
+input: Wwwd Kdfd
+output: kdfd W.
+
+input: Sap is StuPiD
+output: STUPID S.I.
+*/
+
+import java.util.*;
+public class initials{
+	public static void main(String l[]){
+		Scanner nrt=new Scanner(System.in);
+		System.out.println("enter your stupid name>>");
+		String name=((nrt.nextLine()).toUpperCase()).trim(),ans="";
+		// assuming input: "Sap Is Stupid"
+		int last_whitespace=name.lastIndexOf(" ");
+		ans+=(name.substring(last_whitespace,name.length())+" "+((name.charAt(0)))+". ").trim();// returns "Stupid S."
+
+		System.out.println(ans);
+		
+		for(int i=0;i<last_whitespace;i++){
+			if(name.charAt(i)==' '){
+				ans+= name.charAt(i+1)+".";
+			}
+		}
+		System.out.println(ans.trim());
+	}
+}
